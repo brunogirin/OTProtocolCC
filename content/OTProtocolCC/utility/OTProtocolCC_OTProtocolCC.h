b0c2096d98d9385f062131be068619b361bbf824
DamonHD <d@hd.org>
2015-07-28 19:21:57 +0100

    COH-2: wire format 1st pass

--- content/OTProtocolCC/utility/OTProtocolCC_OTProtocolCC.h
+++ content/OTProtocolCC/utility/OTProtocolCC_OTProtocolCC.h
@@ -83,14 +83,9 @@ namespace OTProtocolCC
     //   * rad-open-percent     [0,100] 0-100 in 1% steps, percent open approx to set rad valve (ro)
     //   * light-colour         [0,3] bit flags 1 is red 2 is green (lc)
     //   * light-on-time        [0,15] 0-30s in units of 2s (lt)
-    //   * relative-humidity    [0,100] 0-100 in 1% steps (rh)
-    //   * temperature-ds18b20  [0,199] 0.000-99.999C in 1/2 C steps, pipe temp (tp)
-    //   * temperature-opentrv  [0,1599] 0.000-99.999C in 1/16 C steps, room temp (tr)
-    //   * window               [false,true] false=closed,true=open (w)
-    //   * switch               [0,7] activation count, wrapround, helps async poll detect activation (s)
     //   * Two extension bytes, currently reserved and of value 0.
     // Should generally be fixed length on the wire, and protected by non-zero version of CRC7_5V.
-    //     '?' 10 hc2 hc2 ro lolt rh tp tr1 to2ws 0 0 crc
+    //     '?' 6 hc2 hc2 ro lclt 0 0 crc
     // This representation is immutable.
     struct CC1PollAndCommand
         {
@@ -102,9 +97,16 @@ namespace OTProtocolCC
 
     // CC1PollResponse contains:
     //   * House code (hc1, hc2) of valve controller that the poll/command is being sent to.
-    //   * ... magic ...
+    //   * relative-humidity    [0,100] 0-100 in 1% steps (rh)
+    //   * temperature-ds18b20  [0,199] 0.000-99.999C in 1/2 C steps, pipe temp (tp)
+    //   * temperature-opentrv  [0,1599] 0.000-99.999C in 1/16 C steps, room temp (tr)
+    //   * window               [false,true] false=closed,true=open (w)
+    //   * switch               [0,7] activation count, wrapround, helps async poll detect activation (s)
+    //   * One extension byte, currently reserved and of value 0.
     // Should generally be fixed length on the wire, and protected by non-zero version of CRC7_5V.
-    //     '*' len hc2 hc2 magic* crc
+    //     '*' 7 hc2 hc2 rh tp tr1 tr2ws 0 crc
+    // Note that tr1 is top 8 bits of tr (eg in 1/2C).
+    // Note that tr2wd is bottom 3 bits of tr (eg in 1/16C) + w + 3 s bits
     // This representation is immutable.
     struct CC1PollResponse
         {
