278e167df088815eedbce7ac9c64897bbda419fe
DamonHD <d@hd.org>
2015-07-28 13:29:39 +0100

    COH-2: WIP

--- content/OTProtocolCC/utility/OTProtocolCC_OTProtocolCC.h
+++ content/OTProtocolCC/utility/OTProtocolCC_OTProtocolCC.h
@@ -30,6 +30,41 @@ Author(s) / Copyright (s): Damon Hart-Davis 2015
 // Use namespaces to help avoid collisions.
 namespace OTProtocolCC
     {
+    // From OTRadioLink:
+    //namespace OTRadioLink
+    //    {
+    //    // For V0p2 messages on an FS20 carrier (868.35MHz, OOK, 5kbps raw)
+    //    // the leading byte received indicates the frame type that follows.
+    //    enum FrameType_V0p2_FS20
+    //        {
+    //...
+    //        // Messages for minimal central-control V1 (eg REV9 variant).
+    //        FTp2_CC1Alert                = '!', // 0x21
+    //        FTp2_CC1PollAndCmd           = '?', // 0x3f
+    //        FTp2_CC1PollResponse         = '*', // 0x2a
+    //...
+    //        }
+    //     }
+
+    // General byte-level format of the (CC1) hub/relay messages: type len HC1 HC2 body* crc7nz
+    //
+    // In part to be compatible with existing custom use of the FS20 carrier (but not encoding),
+    // the following will hold:
+    //
+    //  a) The first byte is one of '!', '?' or '*' to indicate the message type for the initial forms.
+    //  b) (The first byte will later be one of '!', '?' or '*' ORed with '0x80' to indicate a secure message variant.)
+    //  c) The second byte is a length byte (nn) of the rest of the frame excluding CRC.
+    //  d) nn bytes of data follow, of which the first two bytes will be the house code.
+    //  e) The 7-bit CRC follows, arranged to never be 0x00 or 0xff.
+    //  f) For the secure forms the message type and length and the house code will be part of the authenticated data.
+    //
+    // Use 7-bit CRC with Polynomial 0x5B (1011011, Koopman) = (x+1)(x^6 + x^5 + x^3 + x^2 + 1) = 0x37 (0110111, Normal).
+    // See: http://users.ece.cmu.edu/~koopman/roses/dsn04/koopman04_crc_poly_embedded.pdf
+    // Should detect all 3-bit errors in up to 7 bytes of payload,
+    // see: http://users.ece.cmu.edu/~koopman/crc/0x5b.txt
+    // For 2 or 3 byte payloads this should have a Hamming distance of 4 and be within a factor of 2 of optimal error detection.
+
+
 
     }
 
